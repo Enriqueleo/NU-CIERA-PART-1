bddbcd8287bef526b1804fd262d83b059507dcaa
Enrique <40580024+Enriqueleo@users.noreply.github.com>
2018-07-13 12:07:11 -0500

    Update AnalyzeFirstdataset.py
    Changed tmp to t. bopt is a way of looking for an optimal b coefficient. Shorten the lines of code by combining functions.

--- Northwestern REU 2018/AnalyzeFirstdataset.py
+++ Northwestern REU 2018/AnalyzeFirstdataset.py
@@ -5,29 +5,26 @@ import pandas as pd
 import time
 tp=pd.read_excel('SN2006Xopt_gFilter_.xlsx',sheet_name='1')
 
-def constant(A,c,d):
-        return (A-A*d)/(1+c)
-def exp1(x,c,b,s):
-        return 1-b*x+c*np.exp(-(x/s)**2)
-def exp2(x,d,k):
-        return 1-d*np.exp(-k*x)
-
+def ef(x,c,b,d,k,s):
+        return (1-b*x+c*np.exp(-(x/s)**2))/(1-d*np.exp(-k*x))
 def fitting(x,A,b,c,d,k,s,t):
-        return (constant(A,c,d)*exp1(x-t,c,b,s))/exp2(x-t,d,k)
+        return ((A-A*d)/(1+c))*ef(x-t,c,b,d,k,s)
 
-tmp=tp['traw']
+t=tp['traw']
 mag=tp['Magnitude']
 t0=tmp[np.where(mag==max(mag))[0]]
-pa,covar=spo.curve_fit(fitting,tmp,mag,[max(mag),0.001,\
+bopt=(mag[len(t)-1]-mag[len(t)-4])/(tmp[len(t)-1]-tmp[len(t)-4])
+pa,covar=spo.curve_fit(fitting,t,mag,[max(mag),-bopt,\
  -0.01,0.001,0.3,20,t0],sigma=tp['Uncertainty'])
 
-x=np.array(range(int(min(tmp)-2),int(max(tmp)+2)))
+x=np.linspace(min(t)-3,max(t)+2,200)
 plt.figure(figsize=(6,6))
 plt.plot(x,fitting(x,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]),'r')
-plt.scatter(tmp,mag)
+plt.scatter(t,mag)
 plt.ylabel('Magnitude')
 plt.xlabel('Epoch (days)')
+plt.title("Light Curve")
 
-parameter=['A','b','c','d','k','sigma','to']
+parameter=['A','b','c','d','k','σ','t0']
 for i in range(len(pa)):
         print(f'{parameter[i]}={pa[i]:4.6}±{np.diag(covar)[i]:4.3}')
