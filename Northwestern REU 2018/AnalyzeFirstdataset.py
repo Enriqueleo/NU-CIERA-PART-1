413e61fec6376d5ce05404d608b20417ea774a5e
Enrique <40580024+Enriqueleo@users.noreply.github.com>
2018-07-13 18:37:02 -0500

    Update AnalyzeFirstdataset.py
    Added some better estimators for finding the amplitude and t of finding the maximum magnitude. Added some new variable names to find the columns of data. Added a reduced chi-square calculation.

--- Northwestern REU 2018/AnalyzeFirstdataset.py
+++ Northwestern REU 2018/AnalyzeFirstdataset.py
@@ -2,29 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as spo
 import pandas as pd
-import time
+#I read in the excel file of the Type Ia Supernova
 tp=pd.read_excel('SN2006Xopt_gFilter_.xlsx',sheet_name='1')
-
-def ef(x,c,b,d,k,s):
-        return (1-b*x+c*np.exp(-(x/s)**2))/(1-d*np.exp(-k*x))
-def fitting(x,A,b,c,d,k,s,t):
-        return ((A-A*d)/(1+c))*ef(x-t,c,b,d,k,s)
-
+#Fitting model equation
+def f(xraw,A,b,c,d,k,s,t):
+    x=xraw-t
+    return ((A-A*d)/(1+c))*((1-b*x+c*np.exp(-(x/s)**2))/(1-d*np.exp(-k*x)))
+#The three col of information from the data
 t=tp['traw']
 mag=tp['Magnitude']
-t0=tmp[np.where(mag==max(mag))[0]]
-bopt=(mag[len(t)-1]-mag[len(t)-4])/(tmp[len(t)-1]-tmp[len(t)-4])
-pa,covar=spo.curve_fit(fitting,t,mag,[max(mag),-bopt,\
- -0.01,0.001,0.3,20,t0],sigma=tp['Uncertainty'])
+err=tp['Uncertainty']
+#Finding the parameters of the model
+t0=t[np.where(mag==max(mag))[0]]
+bopt=(mag[len(t)-1]-mag[len(t)-2])/(t[len(t)-1]-t[len(t)-2])/max(mag)
+pa,covar=spo.curve_fit(f,t,mag,[max(mag),-bopt,\
+ -0.01,0.001,0.3,20,t0],sigma=err)
 
-x=np.linspace(min(t)-3,max(t)+2,200)
-plt.figure(figsize=(6,6))
-plt.plot(x,fitting(x,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]),'r')
-plt.scatter(t,mag)
-plt.ylabel('Magnitude')
-plt.xlabel('Epoch (days)')
-plt.title("Light Curve")
+x=np.linspace(min(t)-3,max(t)+2.5,250)
+chisq=sum(((mag-f(t,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]))/err)**2)/(len(t)-7)
+plt.figure(figsize=(6,5))
+plt.plot(x,f(x,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]),'g')
+plt.errorbar(t,mag,yerr=err,fmt='o',color='black')
+plt.ylabel('Magnitude',Fontsize='14')
+plt.xlabel('Epoch (days)',Fontsize='14')
+plt.title("Light Curve",Fontsize='16')
 
-parameter=['A','b','c','d','k','σ','t0']
-for i in range(len(pa)):
-        print(f'{parameter[i]}={pa[i]:4.6}±{np.diag(covar)[i]:4.3}')
+print(f'𝛘2={chisq:5.3}')
+para=['A','b','c','d','k','σ','t0']
+for i in range(len(pa)): 
+    print(f'{para[i]}={pa[i]:4.4}±{np.diag(covar)[i]:4.3}')
