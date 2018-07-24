1d3fa8501269f7c5d3a696d04a2c163c98c87766
Enrique <40580024+Enriqueleo@users.noreply.github.com>
2018-07-23 20:15:07 -0500

    Update ReadingData.py
    This just reduces the number of lines in the code. We hope to limit the run time of the code.

--- Northwestern REU 2018/ReadingData.py
+++ Northwestern REU 2018/ReadingData.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import scipy.optimize as spo
 import pandas as pd
 import glob
-import errno
 path1 = 'C:/Users/enriq/Desktop/Northwestern REU 2018/DATASET/sub DATA 1/*.txt'
 path2 = 'C:/Users/enriq/Desktop/Northwestern REU 2018/DATASET/sub DATA 2/*.txt'
 path3 = 'C:/Users/enriq/Desktop/Northwestern REU 2018/DATASET/sub DATA 3/*.txt'
@@ -15,6 +14,7 @@ def f(xraw,A,b,c,d,k,s,t):
 files1 = glob.glob(path1)
 files2 = glob.glob(path2)
 files3 = glob.glob(path3)
+
 A=[]
 b=[]
 c=[]
@@ -22,6 +22,7 @@ d=[]
 k=[]
 sig=[]
 tpeak=[]
+chisq=[]
 #Finding the parameters of each data set read in.
 for name in files1:
     F=np.loadtxt(name)
@@ -34,6 +35,7 @@ for name in files1:
     cf,covar=spo.curve_fit(f,t,mag,[max(mag),cf[1],-0.01,0.01,0.28,20,t0],sigma=err)
     cf,covar=spo.curve_fit(f,t,mag,[max(mag),cf[1],cf[2],0.01,0.28,20,t0],sigma=err)
     pa,covar=spo.curve_fit(f,t,mag,[cf[0],cf[1],cf[2],0.01,0.28,20,t0],sigma=err)
+    chisq.append(sum(((mag-f(t,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]))/err)**2)/(len(t)-7))
     A.append(pa[0])
     b.append(pa[1])
     c.append(pa[2])
@@ -50,6 +52,7 @@ for name2 in files2:
     t0=t[np.where(mag==max(mag))[0]]
     bopt=(mag[len(t)-1]-mag[len(t)-2])/(t[len(t)-1]-t[len(t)-2])/max(mag)
     pa,covar=spo.curve_fit(f,t,mag,[max(mag),-bopt,-0.01,0.011,0.27,18,t0],sigma=err)
+    chisq.append(sum(((mag-f(t,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]))/err)**2)/(len(t)-7))
     A.append(pa[0])
     b.append(pa[1])
     c.append(pa[2])
@@ -66,6 +69,7 @@ for name3 in files3:
     t0=t[np.where(mag==max(mag))[0]]
     bopt=(mag[len(t)-1]-mag[len(t)-2])/(t[len(t)-1]-t[len(t)-2])/max(mag)
     pa,covar=spo.curve_fit(f,t,mag,[max(mag),0,-0.01,0.01,0.02,20,t0],sigma=err)
+    chisq.append(sum(((mag-f(t,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]))/err)**2)/(len(t)-7))
     A.append(pa[0])
     b.append(pa[1])
     c.append(pa[2])
@@ -83,52 +87,12 @@ sig=np.array(sig)
 tpeak=np.array(tpeak)
 #This creates a histogram of the parameters from every data set that was 
 #read in.
-
-fig=plt.figure(figsize=(13,12))
-plt.hist(A,bins=np.linspace(-20,-13,29),color='red',label='string')
-plt.xlabel('Bins')
-plt.ylabel('Frequency')
-plt.title('Amplitudes')
-fig.savefig('Amp.pdf')
-
-fig=plt.figure(figsize=(13,12))
-plt.hist(b,bins=np.linspace(min(b),max(b),40),color='black',label='string')
-plt.xlabel('Bins')
-plt.ylabel('Frequency')
-plt.title('b')
-fig.savefig('b.pdf')
-
-fig=plt.figure(figsize=(13,12))
-plt.hist(c,bins=np.linspace(min(c),max(c),50),color='Green',label='string')
-plt.xlabel('Bins')
-plt.ylabel('Frequency')
-plt.title('c')
-fig.savefig('c.pdf')
-
-fig=plt.figure(figsize=(13,12))
-plt.hist(d,bins=np.linspace(min(d),max(d),50),color='red',label='string')
-plt.xlabel('Bins')
-plt.ylabel('Frequency')
-plt.title('d')
-fig.savefig('d.pdf')
-
-fig=plt.figure(figsize=(13,12))
-plt.hist(k,bins=np.linspace(min(k),max(k),50),color='Navy',stacked=True,label='string')
-plt.xlabel('Bins')
-plt.ylabel('Frequency')
-plt.title('k')
-fig.savefig('k.pdf')
-
-fig=plt.figure(figsize=(13,12))
-plt.hist(sig,bins=np.linspace(min(sig),max(sig),50),color='Purple',label='string')
-plt.xlabel('Bins')
-plt.ylabel('Frequency')
-plt.title('σ')
-fig.savefig('sigma.pdf')
-
-fig=plt.figure(figsize=(13,12))
-plt.hist(tpeak,bins=np.linspace(min(tpeak),max(tpeak),50),color='Pink',label='string')
-plt.xlabel('Bins')
-plt.ylabel('Frequency')
-plt.title('t0')
-fig.savefig('tatmax.pdf')
+titles=['Amplitudes','b','c','d','k','σ','t0']
+param=[A,b,c,d,k,sig,tpeak]
+print(files)
+for i in range(len(titles)):
+    fig=plt.figure(figsize=(13,12))
+    plt.hist(param[i],bins=np.linspace(min(param[i]),max(param[i]),60),color='blue',label='string')
+    plt.xlabel('Bins')
+    plt.ylabel('Frequency')
+    plt.title(titles[i])
