9de4d9150dcf4fa6cecc6a6ef72be301bdb29bbf
Enrique <40580024+Enriqueleo@users.noreply.github.com>
2018-07-29 17:37:42 -0500

    Update ReadingData.py
    I modified the code to work with the data sets that we are interested in.

--- Northwestern REU 2018/ReadingData.py
+++ Northwestern REU 2018/ReadingData.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as spo
 import glob
-path1 = 'C:/Users/enriq/Desktop/Northwestern REU 2018/DATASET/sub DATA 1/*.txt'
+path1 = 'C:/Users/enriq/Desktop/Northwestern REU 2018/DATASET/DATA/*.txt'
 
 #I read in the file of the Type Ia Supernova
 #Fitting model equation
@@ -21,15 +21,31 @@ tpeak=[]
 chisq=[]
 #Finding the parameters of each data set read in.
 for name in files1:
-    F=np.loadtxt(name)
-    t=F[:,0]
-    mag=F[:,1]
-    err=F[:,2]
+    if name=="C:/Users/enriq/Desktop/Northwestern REU 2018/DATASET/DATA\SN2005keopt_gFilter_.txt":
+        F=np.loadtxt(name)
+        t=F[:,0][:len(F)-1]
+        mag=F[:,1][:len(F)-1]
+        err=F[:,2][:len(F)-1]
+    elif name=="C:/Users/enriq/Desktop/Northwestern REU 2018/DATASET/DATA\SN2007bdopt_gFilter_.txt":
+        F=np.loadtxt(name)
+        t=F[:,0][:len(F)-2]
+        mag=F[:,1][:len(F)-2]
+        err=F[:,2][:len(F)-2]
+    else:
+        F=np.loadtxt(name)
+        t=F[:,0]
+        mag=F[:,1]
+        err=F[:,2]
+        
     t0=t[np.where(mag==max(mag))[0]]
     bopt=(mag[len(t)-1]-mag[len(t)-3])/(t[len(t)-1]-t[len(t)-3])/max(mag)
-    bopt=-bopt*(1-0.14)/(1-0.02)
-    bnd=np.array([(max(mag)-1.5,bopt-0.4,-1,-5,-5,4,t0-20),(max(mag)+1.5,bopt+0.4,1,5,5,70,t0+20)])
-    pa,covar=spo.curve_fit(f,t,mag,[max(mag),bopt,-0.15,0.02,0.27,20,t0],sigma=err,bounds=bnd,method='trf')
+    bopt=-bopt*(1-0.13)/(1-0.019)
+    possig=t[len(t)-1]-t0
+    lensig=possig/3
+    possig=possig/4
+    bnd=np.array([(max(mag)-1,bopt-0.2,-1,-0.5,0,0,t0-20),(max(mag)+1,bopt+0.2,0.5,1,4,possig+lensig,t0+20)])
+    cf,covar=spo.curve_fit(f,t,mag,[max(mag),bopt,-0.13,0.019,0.3,possig,t0],sigma=err,bounds=bnd,method='trf')
+    pa,covar=spo.curve_fit(f,t,mag,[max(mag),bopt,cf[2],0.02,0.3,possig,cf[6]],sigma=err,bounds=bnd,method='trf')
     
     chisq.append(sum(((mag-f(t,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]))/err)**2)/(len(t)-7))
     A.append(pa[0])
@@ -39,15 +55,15 @@ for name in files1:
     k.append(pa[4])
     sig.append(abs(pa[5]))
     tpeak.append(pa[6])
-    x=np.linspace(min(t)-0.2,max(t)+1,100)
-    plt.figure(figsize=(6,6))
+    x=np.linspace(min(t)-0.4,max(t)+1,150)
+    plt.figure(figsize=(8,6))
     plt.plot(x,f(x,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]),color='purple')
-    plt.errorbar(t,mag,yerr=err,fmt='o',color='red')
+    plt.errorbar(t,mag[:len(mag)],yerr=err,fmt='o',color='navy')
     plt.ylabel('Magnitude',Fontsize='16',color='black')
     plt.xlabel('Epoch (days)',Fontsize='16',color='Black')
     plt.title("Light Curve",Fontsize='18',color='Black')
     plt.title(name)
-    
+
 A=np.array(A)
 b=np.array(b)
 c=np.array(c)
@@ -58,7 +74,7 @@ tpeak=np.array(tpeak)
 mean=[np.mean(A),np.mean(b),np.mean(c),np.median(d),np.median(k),np.median(sig),np.mean(tpeak)]
 #This creates a histogram of the parameters from every data set that was 
 #read in.
-titles=['Amplitudes','b','c','d','k','σ','t0']
+titles=['Amplitudes','b','c','d','k','σ','Time at peak']
 param=[A,b,c,d,k,sig,tpeak]
 for i in range(len(titles)):
     fig=plt.figure(figsize=(9,8))
