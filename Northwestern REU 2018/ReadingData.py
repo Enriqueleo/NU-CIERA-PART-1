d6aa5d393e8d3d4c6f67ea039df9bbe71eda74b3
Enrique <40580024+Enriqueleo@users.noreply.github.com>
2018-08-14 12:13:30 -0500

    Update ReadingData.py
    Changed the path names

--- Northwestern REU 2018/ReadingData.py
+++ Northwestern REU 2018/ReadingData.py
@@ -2,14 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as spo
 import glob
-path1 = 'C:/Users/enriq/Desktop/Northwestern REU 2018/DATASET/DATA/*.txt'
-
-#I read in the file of the Type Ia Supernova
-#Fitting model equation
-def f(xraw,A,b,c,d,k,s,t):
-    x=xraw-t
-    return ((A-A*d)/(1+c))*((1-b*x+c*np.exp(-(x/s)**2))/(1-d*np.exp(-k*x)))
-files1 = glob.glob(path1)
+import pandas as pa
 
 A=[]
 b=[]
@@ -19,61 +12,76 @@ k=[]
 sig=[]
 tpeak=[]
 chisq=[]
-#Finding the parameters of each data set read in.
-for name in files1:
+EachSetOfPara=[]
+
+deltaM=[]
+Mmax=[]
+
+#I read in the file of the Type Ia Supernova
+#Fitting model equation
+def f(xraw,A,b,c,d,k,s,t):
+    x=xraw-t
+    return ((A-A*d)/(1+c))*((1-b*x+c*np.exp(-(x/s)**2))/(1-d*np.exp(-k*x)))
+
+path='More DATA/Graph/*.dat'
+file=glob.glob(path)
+for name in file:
+    t=[]
+    mag=[]
+    err=[]
     F=np.loadtxt(name)
-    t=F[:,0]
-    mag=F[:,1]
-    err=F[:,2]
-        
-    t0=t[np.where(mag==max(mag))[0]]
-    bopt=(mag[len(t)-1]-mag[len(t)-3])/(t[len(t)-1]-t[len(t)-3])/max(mag)
-    bopt=-bopt*(1-0.13)/(1-0.019)
-    possig=t[len(t)-1]-t0
-    lensig=possig/3
+    for i in range(len(F)):
+        if F[i,4]<99:
+            t.append(F[i,0])
+            mag.append(F[i,3])
+            err.append(F[i,4])
+    t=np.array(t)
+    mag=np.array(mag)
+    err=np.array(err)
+    t0=t[np.where(mag==min(mag))[0]]
+    bopt=(mag[len(t)-2]-mag[len(t)-4])/(t[len(t)-2]-t[len(t)-4])/min(mag)
+    bopt=-bopt*(1-0.13)/(1-0.018)
+    possig=t[len(t)-3]-t0
+    lensig=possig/3.3333
     possig=possig/4
-    bnd=np.array([(max(mag)-1,bopt-0.2,-1,-0.5,0,0,t0-20),(max(mag)+1,bopt+0.2,0.5,1,4,possig+lensig,t0+20)])
-    cf,covar=spo.curve_fit(f,t,mag,[max(mag),bopt,-0.13,0.019,0.3,possig,t0],sigma=err,bounds=bnd,method='trf')
-    pa,covar=spo.curve_fit(f,t,mag,[max(mag),bopt,cf[2],0.02,0.3,possig,cf[6]],sigma=err,bounds=bnd,method='trf')
+    bnd=np.array([(min(mag)-1,bopt-0.15,-0.5,-0.3,0,0,t0-15),(min(mag)+1,bopt+0.15,0.5,0.5,2,possig+lensig,t0+15)])
+    cf,covar=spo.curve_fit(f,t,mag,[min(mag),bopt,-0.13,0.018,0.3,possig,t0],sigma=err,bounds=bnd,method='trf')
+    bnd=np.array([(min(mag)-1,cf[1]-0.1,cf[2]-0.1,cf[3]-0.5,0,0,t0-7),(min(mag)+1,cf[1]+0.2,cf[2]+0.1,1,5,possig+lensig,t0+7)])
+    cf,covar=spo.curve_fit(f,t,mag,[min(mag),cf[1],cf[2],cf[3],cf[4],possig,t0],sigma=err,bounds=bnd,method='trf')
+    possig=cf[5]
+    bnd=np.array([(min(mag)-1,cf[1]-0.05,cf[2]-0.1,cf[3]-0.25,0,0,t0-2),(min(mag)+1,cf[1]+0.3,cf[2]+0.1,cf[3]+0.05,1.5,possig+12,t0+2)])
+    pa,covar=spo.curve_fit(f,t,mag,[min(mag),cf[1],cf[2],cf[3],cf[4],possig,t0],sigma=err,bounds=bnd)
     
+    x=np.linspace(min(t)-0.5,max(t)+2,501)
+    plt.figure(figsize=(9,5))
+    plt.errorbar(t-pa[6],mag,yerr=err,fmt='o')
+    plt.plot(x-pa[6],f(x,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]),color='purple')
+    plt.xlabel('Days')
+    plt.ylabel('B')
+    plt.gca().invert_yaxis()
     chisq.append(sum(((mag-f(t,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]))/err)**2)/(len(t)-7))
     A.append(pa[0])
     b.append(pa[1])
     c.append(pa[2])
     d.append(pa[3])
     k.append(pa[4])
-    sig.append(abs(pa[5]))
+    sig.append(pa[5])
     tpeak.append(pa[6])
-    x=np.linspace(min(t)-0.4,max(t)+1,150)
-    plt.figure(figsize=(8,6))
-    plt.plot(x,f(x,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]),color='purple')
-    plt.errorbar(t,mag[:len(mag)],yerr=err,fmt='o',color='navy')
-    plt.ylabel('Magnitude',Fontsize='16',color='black')
-    plt.xlabel('Epoch (days)',Fontsize='16',color='Black')
-    plt.title("Light Curve",Fontsize='18',color='Black')
-    plt.title(name)
-    deltaM.append(-f(pa[6]+15,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6])+f(pa[6],pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]))
-    Mmax.append(-21.726+2.698*(-f(pa[6]+15,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6])+f(pa[6],pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6])))
+    EachSetOfPara.append(pa)
+    deltaM.append(f(pa[6]+15,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6])-f(pa[6],pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6]))
+    Mmax.append(-21.726+2.698*(f(pa[6]+15,pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6])-f(pa[6],pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6])))
 
-A=np.array(A)
-b=np.array(b)
-c=np.array(c)
-d=np.array(d)
-k=np.array(k)
-sig=np.array(sig)
-tpeak=np.array(tpeak)
-mean=[np.mean(A),np.mean(b),np.mean(c),np.median(d),np.median(k),np.median(sig),np.mean(tpeak)]
-#This creates a histogram of the parameters from every data set that was 
-#read in.
-titles=['Amplitudes','b','c','d','k','σ','Time at peak']
-param=[A,b,c,d,k,sig,tpeak]
+titles=['b','c','d','k','σ','Δm15(B)']
+param=[b,c,d,k,sig,deltaM]
 for i in range(len(titles)):
-    fig=plt.figure(figsize=(9,8))
-    plt.hist(param[i],bins=np.linspace(min(param[i]),max(param[i]),100),color='blue',label='string')
+    plt.figure(figsize=(9,5))
+    plt.hist(param[i],bins=np.linspace(min(param[i]),max(param[i]),51),color='red',label='string')
     plt.xlabel('Bins')
     plt.ylabel('Frequency')
     plt.title(titles[i])
-    
-plt.figure()
+
+plt.figure(figsize=(9,5))
 plt.scatter(deltaM,Mmax)
-plt.ylabel('Mmax')
+plt.ylabel('Mmax(B)')
+plt.xlabel('Δm15(B)')
+plt.gca().invert_yaxis()
